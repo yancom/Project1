320ca9b3ed5436b5d7c79ec3d7da6369434a3ba7
yancom <yaniv.abramovich@gmail.com>
2021-10-16 00:09:33 +0300

    Update build.sh

--- Jenkins/build.sh
+++ Jenkins/build.sh
@@ -2,12 +2,12 @@
 
 x=$BUILD_NUMBER
 
-echo "BUILD_NUMBER = $x"
+def build = Thread.currentThread().toString()
+def regexp= ".+?/job/([^/]+)/.*"
+def match = build  =~ regexp
+def jobName = match[0][1]
 
-#def build = Thread.currentThread().toString()
-#def regexp= ".+?/job/([^/]+)/.*"
-#def match = build  =~ regexp
-#def jobName = match[0][1]
+echo "job name =$jobName"
 
 if [ $(expr $x % 3) == "0" ]; then
    echo "succsess"
