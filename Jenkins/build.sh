49194232a97c8c94370cbfe43fa847709f30e3d4
yancom <yaniv.abramovich@gmail.com>
2021-10-16 09:28:33 +0300

    Update build.sh

--- Jenkins/build.sh
+++ Jenkins/build.sh
@@ -3,6 +3,8 @@
 num=$BUILD_NUMBER
 name=$JOB_NAME
 
+echo "This script stared by stage=$STAGE_NAME JOB_BASE_NAME=$JOB_BASE_NAME"
+
 
 if [ $(expr $num % 3) == "0" ]; then
    echo "succsess-> started by $name"
